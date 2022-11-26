7c2037fbb702ff9e258894be3280c2c9a8d5cf09
yancyuu <56645657+yancyuu@users.noreply.github.com>
2022-11-26 18:22:58 +0800

    中台服务中只负责基础配置类

--- client/actor_proxy_client.py
+++ client/actor_proxy_client.py
@@ -3,7 +3,7 @@ import os
 from dapr.actor import ActorProxy, ActorId
 from interface.cookie_actor_interface import CookieActorInterface
 from interface.proxy_actor_interface import ProxyActorInterface
-from interface.spider_actor_interface import SpiderActorInterface
+from interface.spider_setting_actor_interface import SpiderSettingActorInterface
 from interface.fixed_rules_spider_actor_interface import FixedRulesSpiderActorInterface
 
 """
@@ -26,8 +26,8 @@ class ActorProxyClient:
     def create_proxy_actor_proxy(self):
         return ActorProxy.create('ProxyActor', ActorId(self.actor_id), ProxyActorInterface)
 
-    def create_spider_actor_proxy(self):
-        return ActorProxy.create('SpiderActor', ActorId(self.actor_id), SpiderActorInterface)
+    def create_spider_setting_actor_proxy(self):
+        return ActorProxy.create('SpiderSettingActor', ActorId(self.actor_id), SpiderSettingActorInterface)
 
     def create_fixed_rules_spider_actor_proxy(self):
         return ActorProxy.create('FixedRulesSpiderActor', ActorId(self.actor_id), FixedRulesSpiderActorInterface)
